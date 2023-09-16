b5ab05f65d7544882aab3e22f0ce61fe3039e42a
abmasud1214 <abmasud1214@gmail.com>
2023-09-16 14:41:34 -0700

    added header that explains current stage.

--- src/Components/FreeDraw.js
+++ src/Components/FreeDraw.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Canvas from "./canvas";
+import { lengthDefined } from "../Utils/pointsofbox";
 
 export default function FreeDraw(props) {
     const [width, height] = [400, 400]; 
 
     const [boxType, setBoxType] = React.useState("DrawnBox");
 
+    
     const onChangeEvent = (evt) => {
         setBoxType(evt.target.value);
         // console.log(evt.target.value);
     }
-
+    
     const initBox = [[Math.floor(width / 2), Math.floor(height / 2)], ...Array.from(7)]
     const [boxState, setBoxState] = React.useState({
         boxPoints: initBox,
@@ -18,6 +20,23 @@ export default function FreeDraw(props) {
         vanishingPoints: [...Array.from(3)]
     })
 
+    const setInfoHeader = (length) => {
+        if (length <= 3) {
+            return "Draw the Y of the box."
+        } else if (length === 4) {
+            return "Draw the first corner that connects to the Y of the box. \
+            (This sets two of the vanishing points.)"
+        } else if (length === 5) {
+            return "Draw the second corner that connects to the Y of the box. \
+            (One of these lines should connect to a vanishing point set earlier.)"
+        } else if (length === 6) {
+            return "Draw the final outside corner. (Both of these lines should \
+                connect to the vanishing points set earlier.)"
+        } else {
+            return "Draw the final point in the back corner of the box."
+        }
+    }
+    
     const updateBoxState = (stateType, newValue) => {
         setBoxState((prev) => ({
             ...prev,
@@ -39,6 +58,7 @@ export default function FreeDraw(props) {
 
     return (
         <div>
+            <h3>{setInfoHeader(lengthDefined(boxState.boxPoints))}</h3>
             <Canvas
                 width={400}
                 height={400}
